d551e85896516d767bc1a186b7e307703620fd1c
marcosalvaradomartinez <mam02@iesemilidarder.com>
2023-04-27 20:19:39 +0200

    Resuelto el primer problema

--- js/apps.js
+++ js/apps.js
@@ -1,42 +1,34 @@
-const gridGames = document.querySelector('#grid-games')
+const gridGames = document.getElementById("grid-games")
 
 let gamesList = []
 
-async function getGames() {
-    const respuesta = await fetch("./data/games.json");
-    const data = await respuesta.json();
-
-    data.forEach(game => {
-        gridGames.innerHTML += `
-        <div>
-            <img src="${game.imagen}/>
-            <h1>${game.nombre}</h1>
-        </div>
-    `
+function getGames(){
+    fetch('./data/games.json')
+    .then(response => response.json())
+    .then(data =>{
+        gamesList = data;
+        printGames(data);
     })
 }
-// function getGames() {
-//     fetch('./data/games.json')
-//     .then(response => response.json())
-//     .then(data => {
-//         gamesList = data;
-//         printGames(data);
-//     })
-// }
 
-// function printGames(list) {
-//     gridGames.innerHTML = "";
-//     list.forEach(game => {
-//         gridGames.innerHTML += `
-//         <div>
-//             <img src="${game.imagen}/>
-//             <h1>${game.nombre}</h1>
-//         </div>
-//         `
-//     });
-// };
+function printGames(listJSON){
+    gridGames.innerHTML="";
+    listJSON.forEach(game =>{
+        gridGames.innerHTML+=`
+        <article class="card">
+            <div>
+                <img src="./img/${game.imagen}"/>
+                <h1>${game.nombre}</h1>
+            </div>   
+            <div>${game.pegi}</div>
+            <div>${game.desarrollador}</div>    
+        </article>
+        `
+    })
+}
 
 function init(){
     getGames();
 }
+
 init();
