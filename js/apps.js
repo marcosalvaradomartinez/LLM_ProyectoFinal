d5cd6138dddf532a21f4d42950f0533d0fcfbba9
marcosalvaradomartinez <mam02@iesemilidarder.com>
2023-05-03 16:07:21 +0200

    Estrellas dan problemas

--- js/apps.js
+++ js/apps.js
@@ -16,20 +16,21 @@ function printGames(listJSON){
     listJSON.forEach(game =>{
         gridGames.innerHTML+=`
         <article class="card">
-            <form>
-                <p class="clasificacion">
-                    <input id="radio1" type="radio" name="estrellas" value="5">
-                    <label for="radio1">★</label>
-                    <input id="radio2" type="radio" name="estrellas" value="4">
-                    <label for="radio2">★</label>
-                    <input id="radio3" type="radio" name="estrellas" value="3">
-                    <label for="radio3">★</label>
-                    <input id="radio4" type="radio" name="estrellas" value="2">
-                    <label for="radio4">★</label>
-                    <input id="radio5" type="radio" name="estrellas" value="1">
-                    <label for="radio5">★</label>
-                </p>
-            </form>
+        <form>
+            <p class="clasificacion">
+                <input id="radio1" type="radio" name="estrellas" value="5">
+                <label for="radio1">★</label>
+                <input id="radio2" type="radio" name="estrellas" value="4">
+                <label for="radio2">★</label>
+                <input id="radio3" type="radio" name="estrellas" value="3">
+                <label for="radio3">★</label>
+                <input id="radio4" type="radio" name="estrellas" value="2">
+                <label for="radio4">★</label>
+                <input id="radio5" type="radio" name="estrellas" value="1">
+                <label for="radio5">★</label>
+            </p>
+        </form>
+
             <div>
                 <img src="./img/${game.imagen}"/>
                 <h1>${game.nombre}</h1>
@@ -41,15 +42,6 @@ function printGames(listJSON){
     });
     setFavourites();
 }
-
-function setFavourites(){
-    const favouriteFigure = document.querySelectorAll('.fa-star')
-    favouriteFigure.forEach(e =>{
-        e.addEventListener("click", function(){
-            this.classList.toggle("on")
-        })
-    });
-};
 
 function init(){
     getGames();
