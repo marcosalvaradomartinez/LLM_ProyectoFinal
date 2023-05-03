97708296efbc4fed3a5b0171ebfc6d0164349cf2
marcosalvaradomartinez <mam02@iesemilidarder.com>
2023-05-03 17:45:09 +0200

    Cambios

--- js/apps.js
+++ js/apps.js
@@ -18,16 +18,16 @@ function printGames(listJSON){
         <article class="card">
         <form>
             <p class="clasificacion">
-                <input id="radio1" type="radio" name="estrellas" value="5">
-                <label for="radio1">★</label>
-                <input id="radio2" type="radio" name="estrellas" value="4">
-                <label for="radio2">★</label>
-                <input id="radio3" type="radio" name="estrellas" value="3">
-                <label for="radio3">★</label>
-                <input id="radio4" type="radio" name="estrellas" value="2">
-                <label for="radio4">★</label>
-                <input id="radio5" type="radio" name="estrellas" value="1">
-                <label for="radio5">★</label>
+                <input id="game${game.id}-radio1" type="radio" name="estrellas" value="5">
+                <label for="game${game.id}-radio1">★</label>
+                <input id="game${game.id}-radio2" type="radio" name="estrellas" value="4">
+                <label for="game${game.id}-radio2">★</label>
+                <input id="game${game.id}-radio3" type="radio" name="estrellas" value="3">
+                <label for="game${game.id}-radio3">★</label>
+                <input id="game${game.id}-radio4" type="radio" name="estrellas" value="2">
+                <label for="game${game.id}-radio4">★</label>
+                <input id="game${game.id}-radio5" type="radio" name="estrellas" value="1">
+                <label for="game${game.id}-radio5">★</label>
             </p>
         </form>
 
@@ -37,10 +37,10 @@ function printGames(listJSON){
             </div>   
             <div>${game.pegi}</div>
             <div>${game.desarrollador}</div>    
+            <button>Comprar</button>
         </article>
         `
     });
-    setFavourites();
 }
 
 function init(){
